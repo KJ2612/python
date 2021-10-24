0457a995ace1e5e5c551b843caf7d061742eb9aa
root <root@localhost>
2021-10-24 13:19:02 +0530

    auto publish 1635061742411

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
-import random
-maxn = 10
-n = random.randint(1, maxn)
-print('Welcome to guess the number game!')
-print('Guess the number from 1 to %d' % maxn)
-guess = None
-while guess != n:
-    guess = int(input('Your try: '))
-    if guess > n:
-        print('Too high')
-    if guess < n:
-        print('Too low')
-
-print('Congratulations, you won!')
+import time
+print("Hello friend! my name is Kush")
+time.sleep(1)
+name = str(input('What is your name? : '))
+print(f"Hello {name}! ")
+time.sleep(0.7)
+w_b_m_f = str(input('Do you wanna be my friend? : '))
+if w_b_m_f == "yes" or w_b_m_f == "Yes":
+	country = str(input("Where are you from? : "))
+	time.sleep(1)
+	print(f"I m also from {country}!")
+	time.sleep(0.5)
+	hobby = str(input("What is your hobby? : "))
+	time.sleep(1)
+	print(f"""So... we are from {country} and you like {hobby}
+That's soo cool bro!
+I think we are gonna be the best friends.""")
+else:
+	print("Whatever!!!")
